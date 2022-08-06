166977f3fe44e6e293da6f3a9b6d9ddd85f8e8a1
Jino Bae <im@baejino.com>
2022-08-06 23:54:06 +0900

    🏭 Remove remember store

--- server/src/client/src/stores/source.ts
+++ server/src/client/src/stores/source.ts
@@ -1,8 +1,5 @@
 import { createStore } from 'badland'
-import { rememberOnStorage } from 'badland/dist/plugin'
 
 export const sourceStore = createStore({
     data: '',
 })
-
-rememberOnStorage('mymydev__source', sourceStore)
--- server/src/client/src/stores/terminal.ts
+++ server/src/client/src/stores/terminal.ts
@@ -1,8 +1,5 @@
 import { createStore } from 'badland'
-import { rememberOnStorage } from 'badland/dist/plugin'
 
 export const terminalStore = createStore({
     data: '',
 })
-
-rememberOnStorage('mymydev__terminal', terminalStore)
