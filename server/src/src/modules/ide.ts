3b8850729950bfa63c0a10d92627d500928a4d0d
Jino Bae <im@baejino.com>
2023-01-04 20:19:29 +0900

    🔧 Increase time-out time

--- server/src/src/modules/ide.ts
+++ server/src/src/modules/ide.ts
@@ -8,7 +8,7 @@ interface RunCodeOption {
 export function runCode(commands: string[], option?: RunCodeOption) {
     try {
         const result = subprocess.execSync(commands.join(' '), {
-            timeout: 5000
+            timeout: 10000
         })
         if (option?.isCompile) {
             return ''
